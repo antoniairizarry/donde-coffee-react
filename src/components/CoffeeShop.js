3c4257e269d5ee97cf2edd6cf14ca5627e09b9d0
kate myer <kate.d.mangubat@gmail.com>
2020-07-18 20:09:15 -0700

    can display coffee shop info. updated working rts.

--- src/components/CoffeeShop.js
+++ src/components/CoffeeShop.js
@@ -13,18 +13,18 @@ class CoffeeShop extends Component {
   }
 
   render() {
+    let base_url = "/search/"+ this.props.id 
 
     return (
       <div className='coffeeshop-card'>
-        <div className='coffeeshop-image-url'><img src={this.props.image_url} /></div>
-        <div className='coffeeshop-name'>{this.props.name}</div>
+        <a href={base_url}><div className='coffeeshop-image-url'><img src={this.props.image_url}/></div></a>
+        <a href={base_url}><div className='coffeeshop-name'>{this.props.name}</div></a>
         <div className='coffeeshop-address'>{this.props.address}</div>
         <div className='coffeeshop-phone'>{this.props.phone}</div>
         <div className='coffeeshop-price'>{this.props.price}</div>
         <div className='coffeeshop-rating'>{this.props.rating}</div>
         {/* <div className='coffeeshop-distance'>{this.props.distance}</div> */}
       </div>
-     
     );
   }
 
