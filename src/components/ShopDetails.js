ab877f6eb5c0c361bd12a79c99852067dc58c083
kate myer <kate.d.mangubat@gmail.com>
2020-07-20 17:09:26 -0700

    toggle between fave-nofave fx

--- src/components/ShopDetails.js
+++ src/components/ShopDetails.js
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import './ShopDetails.css';
 import CoffeeShop from './CoffeeShop'
+// https://stackoverflow.com/questions/39999367/how-do-i-reference-a-local-image-in-react
+import emptycup from '../images/emptycup.jpeg';
+import filledcup from '../images/filledcup.jpeg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart as fasHeart} from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 class ShopDetails extends Component {
 
@@ -10,7 +17,8 @@ class ShopDetails extends Component {
     super(props);
     
     this.state = {
-      name : null
+      name : null,
+      filled: false
     };
     this.id = this.props.match.params.id
     this.image_url = ''
@@ -18,14 +26,14 @@ class ShopDetails extends Component {
     this.phone = ''
     this.price = ''
     this.rating = null
+    this.cupIcon = emptycup
   
-
   }
   
   componentDidMount() {
       //make a call to api to display shop detail based on id
       axios.get('http://localhost:5000/coffeeshops/' + this.id).then((response)=> {
-        console.log(response)   
+        // console.log(response)   
         this.image_url = response.data.coffeeshops.image_url
         this.address = response.data.coffeeshops.location.display_address
         this.phone = response.data.coffeeshops.display_phone
@@ -33,7 +41,42 @@ class ShopDetails extends Component {
         this.rating = response.data.coffeeshops.rating
         this.setState({name: response.data.coffeeshops.name})
       })
+      //determine when cup is filled
+      //query table, ask flask if favorite exists
+      // GET '/favorites/<user_id>'
+      let user_id = 1
+      axios.get('http://localhost:5000/favorites/' + user_id).then((response)=> {
+        console.log(response) 
+        let shop_ids_array = response.data.favorited_shop_ids
+        // if yelpID exists == filledcup 
+        //loop through shop_ids
+          // Check if a value exists in the shop id array
+          // else, emptycup
+          if(shop_ids_array.includes(this.id)){
+            this.setState({ filled: true });
+         } else{
+            this.setState({ filled: false });
+         }  
+      });  
   }
+
+  myFunction(x) {
+    console.log("here")
+    x.classList.toggle("fa-heart-o");
+  }
+
+  handleClick = () => {
+    this.setState({ filled: !this.state.filled });
+    axios.post("http://localhost:5000/togglefavorite", {      
+        user_id: 1,
+        shop_id: this.id
+    },)
+    .then(response => {
+      console.log(response);
+    })
+  }
+
+
 
   render() {
 
@@ -50,6 +93,9 @@ class ShopDetails extends Component {
           rating={this.rating}
           // distance={distance}
           ></CoffeeShop>
+          <div onClick={this.handleClick} >
+            <FontAwesomeIcon icon={this.state.filled ? fasHeart : faHeart}/>
+          </div>
       </div>
     );
   }
