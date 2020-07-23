98c9ee7cb244769753113ef9c9e4f654e5b69963
Antonia <antonianomamiukor@gmail.com>
2020-07-22 23:48:40 -0700

    added proper route to home button

--- src/components/header.js
+++ src/components/header.js
@@ -18,7 +18,7 @@ class Header extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-background" id="mainNav">
           <div className="container">
-              <a className="navbar-brand js-scroll-trigger" href="#page-top"><img src={Logo}alt="" /></a>
+              <a className="navbar-brand js-scroll-trigger" href="/"><img src={Logo}alt="" /></a>
               <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                   Menu
                   <i className="fas fa-bars ml-1"></i>
